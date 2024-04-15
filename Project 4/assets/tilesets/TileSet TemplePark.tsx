9a7c85640d49987a58df91416ed9d4708517073d
Dawson Turska <dawsonturska@gmail.com>
2024-04-15 18:51:50 -0400

    Set up basic layout, and added in the sprite sheets for the 3 main enemies.

---
+++ Project 4/assets/tilesets/TileSet TemplePark.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TileSet TemplePark" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="Assets - Japanese Style GB/TileSets/TileSet TemplePark.png" width="112" height="104"/>
+</tileset>
